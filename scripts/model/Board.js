202ed2daa32ae3e08600ddd4ac4b579fa49004e1
Kasper Zutterman <kasper.zutterman@ugent.be>
2019-10-18 20:04:30 +0200

    Created Board Class

---
+++ scripts/model/Board.js
@@ -0,0 +1,32 @@
+import Cell from './Cell.js';
+
+export default class Board {
+        
+        constructor (width, height, cellWidth, cellHeight) {
+            this.width = width;
+            this.height = height;
+            this.cellWidth = cellWidth;
+            this.cellHeight = cellHeight;
+            this.grid;
+            this.makeGrid(this.width, this.height);
+            this.createCells(this.width, this.height);
+        }
+        
+        makeGrid(xCell, yCell) {
+            this.grid = [yCell];
+            for (var y = 0; y < yCell; y++) {
+                this.grid[y] = [xCell];
+            }
+        }
+        
+        createCells(xCell, yCell) {
+            for (let y = 0; y < yCell; y++) {
+                for (let x = 0; x < xCell; x++) {
+                    this.grid[y][x] = new Cell(x, y);
+                }
+            }
+        }
+          
+}
+
+
