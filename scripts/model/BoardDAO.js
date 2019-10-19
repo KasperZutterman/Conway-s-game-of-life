f829a16e1dd8267a018025cd4ad0273f553abc5e
Kasper Zutterman <kasper.zutterman@ugent.be>
2019-10-19 09:56:45 +0200

    Added aliva/dead count per generation

--- scripts/model/BoardDAO.js
+++ scripts/model/BoardDAO.js
@@ -8,6 +8,8 @@ export default class BoardDAO {
             this.cellWidth = cellWidth;
             this.cellHeight = cellHeight;
             this.gridData;
+            this.cellsAlive = [];
+            this.cellsDead = [];
             this.makeGridData(this.width, this.height);
             this.generation = 0;
         } 
@@ -29,9 +31,13 @@ export default class BoardDAO {
         }
         
         addData(board) {
+            this.cellsAlive.push(0);
+            this.cellsDead.push(0);
             for (let y = 0; y < board.height; y++) {
                 for (let x = 0; x < board.width; x++) {
                     this.gridData[y][x] += board.grid[y][x].alive ? 1 : 0;
+                    this.cellsAlive[this.generation] += board.grid[y][x].alive ? 1 : 0;
+                    this.cellsDead[this.generation] += board.grid[y][x].alive ? 0 : 1;
                 }
             }
             this.generation++;
